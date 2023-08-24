d6c70a665b7244c223fa1bc381962b4183c26c16
StellaCG <67925940+StellaCG@users.noreply.github.com>
2023-08-24 16:52:16 +0000

    ported main.py and ebay.py from previous replit

---
+++ ebay.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import locale
+from datetime import datetime
+
+locale.setlocale(locale.LC_NUMERIC, 'en_US.UTF-8')
+
+
+class Listing:
+
+  def __init__(self, name, price, days):
+    self.name = name
+    self.price = price
+    self.date = days
+
+  def __str__(self):
+    return f"{self.name}\t({self.price} sold {self.date} ago)"
+
+
+class PriceSummary:
+
+  def __init__(self, avg, min, max):
+    self.avg = avg
+    self.min = min
+    self.max = max
+
+  def __str__(self):
+    return f"\naverage price: {self.avg}\nlowest price: {self.min}\nhighest price: {self.max}"
+
+
+# query = "alocasia azlanii"
+def ebay_search(query, MAX_RESULTS):
+  search_url = "https://www.ebay.com/sch/i.html?_nkw=" + query + "&_Sold=1&LH_Sold=1&LH_Complete=1"
+
+  response = requests.get(search_url)
+  soup = bs(response.text, 'html.parser')
+
+  listings = []
+  names = soup.select('.s-item__title span[role="heading"]')
+  prices = soup.select('.s-item__detail--primary span[class="s-item__price"]')
+  days_dt = soup.select('.s-item__title--tag span[class="POSITIVE"]')
+
+  for i in range(1, len(names)):
+    listings.append(
+      Listing(
+        names[i].text, prices[i].text,
+        datetime.today() -
+        datetime.strptime(days_dt[i - 1].text[6:], '%b %d, %Y')))
+
+  average_price = 0.00
+  lowest_price = 99999.00
+  highest_price = 0.00
+  parsed_price = 0.00
+
+  # cap max results
+  if MAX_RESULTS > len(listings):
+    MAX_RESULTS = len(listings)
+  
+  for i in range(1, MAX_RESULTS):
+    # check for price ranges and default to lowest
+    if "to" in prices[i].text:
+      parsed_price = locale.atof(prices[i].text.split()[0][1:])
+    else:
+      parsed_price = locale.atof(prices[i].text[1:])
+    average_price += parsed_price
+    if parsed_price > highest_price:
+      highest_price = parsed_price
+    if parsed_price < lowest_price:
+      lowest_price = parsed_price
+
+  average_price /= MAX_RESULTS
+  pricesum = PriceSummary(round(average_price, 2), lowest_price, highest_price)
+  return (listings, pricesum)
+  '''for i in range(0,MAX_RESULTS):
+    print(listings[i])'''
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('hello world')
+from ebay import *
+# from matplotlib import pyplot as plt
+
+# todo: walmart, amazon
+# fix bug in line 20 with price ranges
+
+# removed graphing for now
+
+def main():
+  query = input("Enter a search term: ")
+  numresults = int(input("\nHow many results: "))
+  results = ebay_search(query, numresults)
+
+  if numresults > len(results[0]):
+    numresults = len(results[0])
+  for i in range(numresults):
+    print(results[0][i])
+  print(results[1])
+
+  print(f"\nresults shown: {numresults}")
+
+if __name__ == "__main__":
+  main()
+
+
+
+'''plt.scatter([i for i in range(1, numresults+1)], [float(l.price[1:]) for l in results[0][:numresults]])
+  price_over_time = []
+  for i in range(numresults+1):
+    price_over_time.append([results[0][i].date.days, float(results[0][i].price[1:])])
+
+  plt.plot([p[0] for p in price_over_time], [p[1] for p in price_over_time])
+  # plt.plot([d.date.days for d in results[0][:numresults]], [float(l.price[1:]) for l in results[0][:numresults]])
+  plt.show()'''
